211d54ce769d0d41a82de8354010818b6dce35ac
Budi Irawan <deerawan@gmail.com>
2016-10-01 20:31:21 +0700

    add session using redis and initialize passport

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const passport = require('passport');
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(session({
+  store: new RedisStore()
+}));
 
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!');
