0f507e6b6ee205af418583f427bdafbb27316860
Budi Irawan <deerawan@gmail.com>
2016-10-04 23:28:58 +0700

    add user repository

---
+++ app/repositories/userRepository.js
@@ -0,0 +1,14 @@
+
+var UserRepository = function() {
+  var getNextUserId = function() {
+
+  };
+  var isUserExist = function() {
+
+  };
+  var createUser = function() {
+
+  };
+};
+
+module.exports = UserRepository;
