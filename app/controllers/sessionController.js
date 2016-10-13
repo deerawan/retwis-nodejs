7a7c66f757ad864565c6bfd6a1bc8c2840d09c6b
Budi Irawan <deerawan@gmail.com>
2016-10-13 21:59:42 +0700

    try to integrate rxjs

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
 const redisClient = redis.createClient();
+const UserRepository = require('../repositories/userRepository');
 
 redisClient.on("error", function (err) {
   console.log("Error " + err);
@@ -11,19 +12,22 @@ var SessionController = function() {
   }
   var register = (req, res) => {
     var username = req.body.username;
-    console.log(username);
-    redisClient.hget('users', username, (err, user) => {
-      console.log(res);
-      if (user) {
-        res.send('user exist');
-      } else {
-        redisClient.incr('next_user_id', (err, nextUserId) => {
-          console.log(nextUserId);
-          redisClient.hset('users', username, nextUserId);
-          res.send('created');
-        });
-      }
-    });
+    var userRepo = new UserRepository(redisClient);
+    userRepo.isUserExist(username);
+
+    // console.log(username);
+    // redisClient.hget('users', username, (err, user) => {
+    //   console.log(res);
+    //   if (user) {
+    //     res.send('user exist');
+    //   } else {
+    //     redisClient.incr('next_user_id', (err, nextUserId) => {
+    //       console.log(nextUserId);
+    //       redisClient.hset('users', username, nextUserId);
+    //       res.send('created');
+    //     });
+    //   }
+    // });
 
   }
 
--- app/repositories/userRepository.js
+++ app/repositories/userRepository.js
@@ -1,14 +1,31 @@
+var Rx = require('rxjs/Rx');
 
-var UserRepository = function() {
+var UserRepository = function(redisClient) {
   var getNextUserId = function() {
 
   };
-  var isUserExist = function() {
+  var isUserExist = function(username) {
+    redisClient.hget('users', username, (err, user) => {
+      console.log(user);
+    });
+    // var hget = Rx.Observable.fromCallback(redisClient.hget);
 
+    var hget = Rx.Observable.bindNodeCallback(redisClient.hget.bind(redisClient))
+    var source = hget('users', username);
+
+    var subscription = source.subscribe(
+      function (x) { console.log('onNext: %s', x); },
+      function (e) { console.log('onError: %s', e); },
+      function ()  { console.log('onCompleted'); }
+    );
   };
   var createUser = function() {
 
   };
+
+  return {
+    isUserExist: isUserExist
+  }
 };
 
 module.exports = UserRepository;
