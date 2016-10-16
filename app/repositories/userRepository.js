668cac27eb7bb5b65a8562973936763cc9b3b527
Budi Irawan <deerawan@gmail.com>
2016-10-16 22:08:44 +0700

    update flow

--- app/repositories/userRepository.js
+++ app/repositories/userRepository.js
@@ -6,37 +6,40 @@ var UserRepository = function(redisClient) {
   var hset = Rx.Observable.bindNodeCallback(redisClient.hset.bind(redisClient));
 
   var register = function(username) {
-    return hget('users', username)
+    return getUser(username)
       .map((user) => {
+        console.log(user);
         if (!user) {
-          return incr('next_user_id');
+          return getNextUserId();
         }
       })
       .map((nextUserId) => {
+        console.log(nextUserId);
         return hset('users', username, nextUserId);
       });
   }
   var getNextUserId = function() {
     var incr = Rx.Observable.bindNodeCallback(redisClient.incr.bind(redisClient));
-    var source = incr('next_user_id');
-    var subscription = source.subscribe(
-
-    );
+    return incr('next_user_id');
+  };
+  var getUser = function(username) {
+    var hget = Rx.Observable.bindNodeCallback(redisClient.hget.bind(redisClient));
+    return hget('users', username);
   };
   var isUserExist = function(username) {
-    redisClient.hget('users', username, (err, user) => {
-      console.log(user);
-    });
-    // var hget = Rx.Observable.fromCallback(redisClient.hget);
+    // redisClient.hget('users', username, (err, user) => {
+    //   console.log(user);
+    // });
+    // // var hget = Rx.Observable.fromCallback(redisClient.hget);
 
-    var hget = Rx.Observable.bindNodeCallback(redisClient.hget.bind(redisClient));
-    var source = hget('users', username);
+    // var hget = Rx.Observable.bindNodeCallback(redisClient.hget.bind(redisClient));
+    // var source = hget('users', username);
 
-    var subscription = source.subscribe(
-      (user) => console.log('onNext: %s', x),
-      (e) => console.log('onError: %s', e),
-      ()  => console.log('onCompleted')
-    );
+    // var subscription = source.subscribe(
+    //   (user) => console.log('onNext: %s', x),
+    //   (e) => console.log('onError: %s', e),
+    //   ()  => console.log('onCompleted')
+    // );
   };
   var createUser = function() {
 
